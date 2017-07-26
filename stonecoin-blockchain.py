510d8997054f028d8fd3c945ed1a3ad84bc1f6ad
root <lucashenrique.he@gmail.com>
2017-07-26 11:30:01 -0300

    Fixing a issue with genesis block

--- stonecoin-blockchain.py
+++ stonecoin-blockchain.py
@@ -26,7 +26,7 @@ def next_block(last_block):
 	this_index = last_block.index + 1
 	this_timestamp = date.datetime.now()
 	this_data = "A wild Block appears: " + str(this_index)
-	this_hash = last_block.this_hash
+	this_hash = last_block.hash
 
 	return Block(this_index, this_timestamp, this_data, this_hash)
 	
@@ -38,7 +38,7 @@ def create_genesis_block():
 blockchain = [create_genesis_block()]
 previous_block = blockchain[0]
 
-blocks_to_add = 30
+blocks_to_add = 10
 
 for i in range(0, blocks_to_add):
 	block_to_add = next_block(previous_block)
