d71254a61432422f5bf9734bff089fca4f73b9fb
root <lucashenrique.he@gmail.com>
2017-07-26 11:24:11 -0300

    Configuring all in a single file - for testing purposes

--- stonecoin-blockchain.py
+++ stonecoin-blockchain.py
@@ -1,3 +1,40 @@
+import hashlib as hasher
+import datetime as date
+
+# Creating the block
+class Block:
+	def __init__(self, index, timestamp, data, previous_hash):
+		self.index = index
+		self.timestamp = timestamp
+		self.data = data
+		self.previous_hash = previous_hash
+		self.hash = self.hash_block()
+
+	def hash_block(self):
+		sha = hasher.sha256()
+		sha.update(
+			str(self.index) +
+			str(self.timestamp) +
+			str(self.data) +
+			str(self.previous_hash)
+		)
+
+		return sha.hexdigest()
+
+# Creating a new block
+def next_block(last_block):
+	this_index = last_block.index + 1
+	this_timestamp = date.datetime.now()
+	this_data = "A wild Block appears: " + str(this_index)
+	this_hash = last_block.this_hash
+
+	return Block(this_index, this_timestamp, this_data, this_hash)
+	
+# Creating the genesis block
+def create_genesis_block():
+	return Block(0, date.datetime.now(), "Genesis Block", "0")
+
+# Starting the blockchain
 blockchain = [create_genesis_block()]
 previous_block = blockchain[0]
 
--- stonecoin-genesis.py
+++
@@ -1,4 +0,0 @@
-import datetime as date
-
-def create_genesis_block():
-	return Block(0, date.datetime.now(), "Genesis Block", "0")
